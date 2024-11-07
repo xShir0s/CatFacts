c1db02782a8c097e7291830632058c8a7848d4a3
xShir0s <mtodys2006@gmail.com>
2024-11-07 09:17:37 +0100

    dodanie plików catfacts

---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace CatFacts
+{
+    public class Facts
+    {
+        public string? fact { get; set; }
+        public int? length { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            string json;
+            string url = "https://catfact.ninja/fact?max_length=" + Int32.Parse(Limit.Text);
+
+            using (var webClient = new WebClient())
+            {
+                json = webClient.DownloadString(url);
+            }
+
+            Facts catfacts = JsonSerializer.Deserialize<Facts>(json);
+
+            Fact.Text = "Ciekawostka: " + catfacts.fact + "\n Ilosc znaków:  " + catfacts.length.ToString();
+
+        }
+
+    }
+
+}
